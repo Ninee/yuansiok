2572c3f09b9c7b76eff45175f71036da03c56c9d
XuGe <xu@yuansi.com>
2020-12-12 19:22:40 +0800

    '修复误传百度联调问题'

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -15,17 +15,19 @@ class VisitorController extends Controller
         //首次访问, 区分平台是否需要联调
         switch ($request->platform) {
             case Visitor::PLATFORM_BAIDU:
-                $actived = Visitor::where('platform', Visitor::PLATFORM_BAIDU)->whereNotNull('bd_vid')->where('page_id', $request->page_id)->first();
-                if (!$actived) {
-                    $page = \App\TouTiao::find($request->page_id);
-                    $token = $page->baidu_clue;
-                    $cv = array(
-                        'logidUrl' => $request->url, // 您的落地页url
-                        'newType' => 19 // 转化类型请按实际情况填写
-                    );
-                    $conversionTypes = array($cv);
-                    $ocpc = new BaiduOcpc();
-                    $ocpc->sendConvertData($token, $conversionTypes);
+                if ($request->get('bd_vid')) {
+                    $actived = Visitor::where('platform', Visitor::PLATFORM_BAIDU)->whereNotNull('bd_vid')->where('page_id', $request->page_id)->first();
+                    if (!$actived) {
+                        $page = \App\TouTiao::find($request->page_id);
+                        $token = $page->baidu_clue;
+                        $cv = array(
+                            'logidUrl' => $request->url, // 您的落地页url
+                            'newType' => 19 // 转化类型请按实际情况填写
+                        );
+                        $conversionTypes = array($cv);
+                        $ocpc = new BaiduOcpc();
+                        $ocpc->sendConvertData($token, $conversionTypes);
+                    }
                 }
                 break;
             case Visitor::PLATFORM_TOUTIAO:
